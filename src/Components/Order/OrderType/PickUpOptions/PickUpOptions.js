8ea7e289d1eabd577f404968c938dd4fa7f50bee
Jason Carcamo <jasoncarcamo30@yahoo.com>
2020-04-16 23:12:33 -0400

    Installed datetime picker

--- src/Components/Order/OrderType/PickUpOptions/PickUpOptions.js
+++ src/Components/Order/OrderType/PickUpOptions/PickUpOptions.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Text, View, Button} from "react-native";
+import {Text, View, Button, TextInput} from "react-native";
 import AppContext from "../../../../Services/Context/AppContext/AppContext";
 
 export default class PickUpOptions extends React.Component{
@@ -9,23 +9,90 @@ export default class PickUpOptions extends React.Component{
             orderType: "Pick up",
             time: "",
             address: "",
-            mobileNumber: ""
+            mobileNumber: "",
+            setOptions: false,
+            confirmOptions: false
         }
     }
 
     static contextType = AppContext;
+    
+    asapPickUp = ()=>{
+        const asapPickUp = {
+            orderType: this.state.orderType,
+            time: this.state.time,
+            address: this.state.address,
+            mobileNumber: this.state.mobileNumber
+        };
+
+        this.context.orderContext.setOrderType(asapPickUp);
+    }
+
+    renderOrderTime = ()=>{
+        return (
+            <>
+                <Button
+                    title="ASAP"
+                    onPress={()=>this.setState({ 
+                        setOptions: true,
+                        time: "ASAP"
+                    })}></Button>
+
+                <Button
+                    title="Later"
+                    onPress={()=>this.setState({
+                        setOptions: true,
+                        time: "Later"
+                    })}></Button>
+            </>
+        )
+    }
+
+    setUserOptions = ()=>{
+        return (
+            <View>
+                <TextInput
+                    placeholder="Time"
+                    value={this.state.time}></TextInput>
+                <TextInput
+                    placeholder="Address"></TextInput>
+                <TextInput
+                    placeholder="Mobile number"></TextInput>
+                
+                <Button
+                    title="Ok"
+                    onPress={()=>this.setState({
+                        setOptions: false,
+                        confirmOptions: true
+                    })}></Button>
+            </View>
+        )
+    }
+
+    renderConfirmOptions = ()=>{
+        return (
+            <View>
+                <Text>Is the address: {this.state.address} correct?</Text>
+                
+                <Button
+                    title="Yes"></Button>
+                
+                <Button
+                    title="Edit address"></Button>
+            </View>
+        )
+    }
 
     render(){
 
         return (
             <View>
-                <Text>Pick up</Text>
 
-                <Button
-                    title="ASAP"></Button>
+                {!this.state.setOptions && !this.state.confirmOptions ? this.renderOrderTime() : <View></View>}
 
-                <Button
-                    title="Later"></Button>                
+                {this.state.setOptions ? this.setUserOptions() : <View></View>}
+
+                {this.state.confirmOptions ? this.renderConfirmOptions() : <View></View>}
             </View>
         )
     }
